fa1629d7e56bf1f09c054959f935d0d8ee3a78f2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-06 14:22:54 +0330

    refactor FormError add error.message

--- src/components/ui/FormError.tsx
+++ src/components/ui/FormError.tsx
@@ -1,19 +1,19 @@
-// src/components/ui/FormError.tsx
-import React from "react";
-import type { FieldError } from "react-hook-form";
+import React from 'react'
+import type { FieldError } from 'react-hook-form'
 
 interface FormErrorProps {
   error?: string | FieldError;
 }
 
-const FormError: React.FC<FormErrorProps> = ({ error }) => {
+const FormError: React.FC<FormErrorProps> = ({
+  error
+}) => {
   if (!error) return null;
-
   return (
-    <p className="text-xs text-red-600 mt-1">
-      {typeof error === "string" ? error : error.message}
+    <p className='text-sm text-red-500 mt-1'>
+      {typeof error === "string" ? error : error.message || "خطا"}
     </p>
-  );
-};
+  )
+}
 
-export default FormError;
+export default FormError
