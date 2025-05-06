b494f3e8c09211f92141c58440026adc894d1f47
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-06 12:51:26 +0330

    update page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import "./App.css";
 
-function App() {
+
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import PersonalInfo from './pages/PersonalInfo';
+
+import "./App.css";
+import Skils from './pages/Skils';
+
+const App: React.FC = () => {
   return (
-    <>
-      <p className="text-red-500 text-center">استخدام</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/personal-info" />} />
+        <Route path="/personal-info" element={<PersonalInfo />} />
+        <Route path="/skils" element={<Skils />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
