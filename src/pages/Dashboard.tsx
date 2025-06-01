6019c67f720c7aed1c37350bbc765746e60244aa
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-01 18:23:12 +0330

    update

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,18 @@
+// src/pages/Dashboard.tsx
+import React from "react";
+
+
+const Dashboard: React.FC = () => {
+  return (
+    
+      <div className="text-center">
+        <h2 className="text-xl font-bold mb-4">به داشبورد خوش آمدید!</h2>
+        <p className="text-gray-600">
+          برای شروع، یکی از تب‌های بالا رو انتخاب کنید.
+        </p>
+      </div>
+
+  );
+};
+
+export default Dashboard;
