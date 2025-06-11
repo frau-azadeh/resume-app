dbc6dc7038edb3045db55bcc5beaccfbb526baca
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-11 23:03:01 +0330

    update

---
+++ src/types/types.ts
@@ -0,0 +1,54 @@
+// types.ts
+
+export interface PersonalInfoForm {
+    firstName: string;
+    lastName: string;
+    nationalCode: string;
+    birthDate: string;
+    birthCity: string;
+    birthProvince: string;
+    idNumber: string;
+    issueCity: string;
+    issueProvince: string;
+    religion: string;
+    maritalStatus: string;
+    gender: string;
+    fatherName: string;
+    fatherJob: string;
+    fatherEducation: string;
+    motherName: string;
+    motherJob: string;
+    motherEducation: string;
+    siblingsCount: number;
+    childrenCount: number;
+    residenceProvince: string;
+    residenceCity: string;
+    address: string;
+    postalCode: string;
+    phone: string;
+    email: string;
+    emergencyContactName: string;
+    emergencyContactRelation: string;
+    emergencyContactPhone: string;
+  }
+
+  export type Proficiency = "ضعیف" | "متوسط" | "عالی";
+
+export interface SkillItem {
+  name: string;
+  level: number;
+}
+
+export interface LanguageSkill {
+  language: string;
+  reading: Proficiency;
+  writing: Proficiency;
+  speaking: Proficiency;
+  comprehension: Proficiency;
+}
+
+export interface ManagementSkill {
+  name: string;
+  level: number;
+}
+  
