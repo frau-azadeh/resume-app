3bb20ef0f2fba33228a028a9d8d407aaff9c07a3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-12 21:33:41 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-// src/App.tsx
+// App.tsx
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Layout from "./components/Layout";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import PersonalInfo from "./pages/PersonalInfo";
+import EducationHistory from "./pages/EducationHistory";
+import WorkInfo from "./pages/WorkInfo";
+import SkillForm from "./pages/SkillForm";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Toaster position="top-center" />
-      <Layout />
+      <ToastContainer position="top-center" />
+      <Routes>
+        <Route path="/" element={<Navigate to="/form/personal-info" replace />} />
+        <Route path="/form" element={<Layout />}>
+          <Route path="personal-info" element={<PersonalInfo />} />
+          <Route path="education" element={<EducationHistory />} />
+          <Route path="work-experience" element={<WorkInfo />} />
+          <Route path="skill" element={<SkillForm />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
