e561c3437e49742feab89cd50097a9df5a1d79ac
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-04 23:12:11 +0330

    update type

--- src/validation/educationSchema.ts
+++ src/validation/educationSchema.ts
@@ -6,18 +6,18 @@ export const educationSchema = z.object({
   degree: z.string().min(1, "وارد کردن مقطع الزامی است"),
   field: z.string().min(1, "وارد کردن رشته الزامی است"),
   specialization: z.string().min(1, "وارد کردن گرایش الزامی است"),
-  institutionType: z.string().min(1, "وارد کردن نوع موسسه الزامی است"),
-  institutionName: z.string().min(1, "وارد کردن نام موسسه الزامی است"),
+  institution_type: z.string().min(1, "وارد کردن نوع موسسه الزامی است"),
+  institution_name: z.string().min(1, "وارد کردن نام موسسه الزامی است"),
   grade: z.string().min(1, "وارد کردن معدل الزامی است"),
-  startDate: z.custom<Dayjs>((val) => dayjs.isDayjs(val), {
+  start_date: z.custom<Dayjs>((val) => dayjs.isDayjs(val), {
     message: "تاریخ شروع معتبر نیست",
   }),
-  endDate: z
+  end_date: z
     .custom<Dayjs | null>((val) => val === null || dayjs.isDayjs(val), {
       message: "تاریخ پایان معتبر نیست",
     })
     .nullable(),
-  isStudying: z.boolean(),
+  is_studying: z.boolean(),
   description: z.string().min(1, "وارد کردن توضیحات الزامی است"),
 });
 
