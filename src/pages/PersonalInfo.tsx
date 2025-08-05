8ba56c3a9c947bc15f11355154937a9e2a11d946
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-05 22:58:18 +0330

    update type

--- src/pages/PersonalInfo.tsx
+++ src/pages/PersonalInfo.tsx
@@ -1,12 +1,11 @@
-// PersonalInfoPage.tsx
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { fetchPersonalInfo } from "../store/slices/personalInfoSlice";
 import PersonalInfoForm from "../components/PersonalInfo/PersonalInfoForm";
 import DateObject from "react-date-object";
 
+import gregorian from "react-date-object/calendars/gregorian";
 import persian from "react-date-object/calendars/persian";
-import persian_fa from "react-date-object/locales/persian_fa";
 
 const PersonalInfoPage: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -19,16 +18,17 @@ const PersonalInfoPage: React.FC = () => {
   return (
     <div className="max-w-5xl mx-auto p-6 bg-white rounded-lg shadow" dir="rtl">
       <h1 className="text-2xl font-bold text-center mb-6">اطلاعات فردی</h1>
+
       <PersonalInfoForm
         defaultValues={{
           ...personalInfo,
           birth_date: personalInfo.birth_date
             ? new DateObject({
                 date: personalInfo.birth_date,
-                format: "YYYY-MM-DD",
-                calendar: persian,
-                locale: persian_fa,
-              }).format("YYYY-MM-DD") // ✅ تبدیل به string
+                calendar: gregorian,
+              })
+                .convert(persian)
+                .format("YYYY-MM-DD")
             : "",
         }}
       />
