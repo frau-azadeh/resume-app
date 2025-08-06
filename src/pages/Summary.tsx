0be26a3ce7532ddba800d5fee6989feebee21f91
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-06 19:21:52 +0330

    update with and css

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,7 +1,9 @@
 export default function summary() {
   return (
-    <div>
-      <h1>اطلاعات شما با موفقیت در بانک استخدامی ما ثبت گردید.</h1>
+    <div className="max-w-5xl mx-auto p-6 bg-white rounded-lg shadow" dir="rtl">
+      <div className="flex flex-col min-h-screen"> 
+        <h1>اطلاعات شما با موفقیت در بانک استخدامی ما ثبت گردید.</h1>
+      </div>
     </div>
   );
 }
