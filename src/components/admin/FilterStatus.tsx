05ec13faa92112fd6ad371de20e11e179b75e21a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-08 17:49:07 +0330

    update css

--- src/components/admin/FilterStatus.tsx
+++ src/components/admin/FilterStatus.tsx
@@ -1,23 +1,21 @@
-// src/components/admin/FilterStatus.tsx
+import type{ StatusFilter } from "../../types/admin";
+
 interface FilterStatusProps {
-    value: string;
-    onChange: (val: string) => void;
-  }
-  
-  const FilterStatus = ({ value, onChange }: FilterStatusProps) => {
-    return (
-      <select
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        className=" border border-gray-300 rounded-md p-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        >
-        <option value="all">همه وضعیت‌ها</option>
-        <option value="pending">در انتظار</option>
-        <option value="approved">تأیید شده</option>
-        <option value="rejected">رد شده</option>
-      </select>
-    );
-  };
-  
-  export default FilterStatus;
-  
+  value: StatusFilter;
+  onChange: (val: StatusFilter) => void;
+}
+
+export default function FilterStatus({ value, onChange }: FilterStatusProps) {
+  return (
+    <select
+      value={value}
+      onChange={(e) => onChange(e.target.value as StatusFilter)}
+      className=" border border-gray-300 rounded-md p-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      >
+      <option value="all">همه</option>
+      <option value="pending">در انتظار</option>
+      <option value="approved">تأیید</option>
+      <option value="rejected">رد</option>
+    </select>
+  );
+}
