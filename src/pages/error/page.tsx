eb6015e1e40e20d95f3133630bf5ca5798eb6f93
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-20 16:20:46 +0330

    change name page

--- src/pages/error/page.tsx
+++ src/pages/error/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const page:React.FC = () => {
+const ErrorPage: React.FC = () => {
   return (
-    <div className='flex items-center justify-center'>
-        <h1 className='text-red-500 '>!Oops please try again later</h1>
+    <div className="flex items-center justify-center">
+      <h1 className="text-red-500 ">!Oops please try again later</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default ErrorPage;
